fc8f8f3a5c2dda10519485b38664952d58b5c2b0
Anib04 <belloanibal16@gmail.com>
2025-08-04 00:44:01 -0400

    Configuracion inicial

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <v-toolbar-title> Mi E-Commerce </v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+    <v-footer class="d-flex flex-column" color="teal" rounded="lg" height="10px">
+      <div class="d-flex w-100 align-center px-4 py-2">
+        <strong>Get connected with us on social networks!</strong>
+
+        <div class="d-flex ga-2 ms-auto">
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            :icon="icon"
+            size="small"
+            variant="plain"
+          ></v-btn>
+        </div>
+      </div>
+
+      <div class="px-4 py-2 bg-surface-variant text-center w-100 rounded-lg">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script setup>
+const icons = ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram']
+</script>
+
+<style></style>
